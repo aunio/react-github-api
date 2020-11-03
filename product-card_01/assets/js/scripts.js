005e19d448b34a56dac881071f4d07f67919bae6
Aunio Ribeiro <aunioribeiro@gmail.com>
2020-11-03 00:41:35 -0300

    product-card__01 | Rotate no card com hover com escala correta

--- product-card_01/assets/js/scripts.js
+++ product-card_01/assets/js/scripts.js
@@ -2,8 +2,8 @@ const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 
 container.addEventListener('mousemove', (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
 
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 })
