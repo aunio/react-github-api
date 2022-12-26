207c44d7248c6bd3d1f8ed87422e905f40f6dd38
Aunio Ribeiro <aunioribeiro@gmail.com>
2022-12-26 03:17:30 -0300

    create handle click

--- react_click-challenge/src/App.js
+++ react_click-challenge/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([])
+
+  const handleClick = () => {
+    console.log('clicou')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id="page"
+      onClick={handleClick}
+    >
+      <span className='dot' />
     </div>
   );
 }
