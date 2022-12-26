1065e2f2d2c49ea071cfd0aaa97b1482b3f998ad
Aunio Ribeiro <aunioribeiro@gmail.com>
2022-12-26 03:44:49 -0300

    Undo and Redo function

--- react_click-challenge/src/App.js
+++ react_click-challenge/src/App.js
@@ -3,24 +3,51 @@ import './App.css';
 
 function App() {
   const [list, setList] = useState([])
+  const [undidList, setUndidList] = useState([])
 
   const handleClick = (event) => {
-    const newDot = {
+    const NEW_DOT = {
       x: event.clientX,
       y: event.clientY,
     }
 
-    setList((prev) => [...prev, newDot])
+    setList((prev) => [...prev, NEW_DOT])
+    setUndidList([])
+  }
 
+  const handleUndo = (event) => {
+    event.stopPropagation()
+
+    if (list.length === 0) {
+      return
+    }
+
+    const LAST_ITEM = list[list.length - 1]
+
+    setList((prev) => [...prev].slice(0, -1))
+    setUndidList((prev) => [...prev, LAST_ITEM])
+  }
+
+  const handleRedo = (event) => {
+    event.stopPropagation()
+
+    if (undidList.length === 0) {
+      return
+    }
+
+    const LAST_ITEM = undidList[undidList.length - 1]
+
+    setUndidList((prev) => [...prev].slice(0, -1))
+    setList((prev) => [...prev, LAST_ITEM])
   }
 
   return (
-    <div
-      id="page"
-      onClick={handleClick}
-    >
-      {list.map(item =>
+    <div id="page" onClick={handleClick}>
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
+      {list.map((item, index) =>
         <span
+          key={index}
           className='dot'
           style={{ top: item.y, left: item.x }}
         />
